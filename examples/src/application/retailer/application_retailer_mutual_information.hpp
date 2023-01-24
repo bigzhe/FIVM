d7718d8d511752af5da2cc03715c277b2e398cd7
Haozhe Zhang <imhaozhe@gmail.com>
2023-01-24 19:10:13 +0100

    chow liu tree construction

--- examples/src/application/retailer/application_retailer_mutual_information.hpp
+++ examples/src/application/retailer/application_retailer_mutual_information.hpp
@@ -1,5 +1,5 @@
-#ifndef APPLICATION_RETAILER_REGRESSION_HPP
-#define APPLICATION_RETAILER_REGRESSION_HPP
+#ifndef APPLICATION_RETAILER_CHOWLIUTREE_HPP
+#define APPLICATION_RETAILER_CHOWLIUTREE_HPP
 
 #include <cmath>
 #include <iomanip>
@@ -7,18 +7,14 @@
 #include "csvreader.hpp"
 #include "clt.hpp"
 
-// BGDSolver<39, 3> bgd_solver;
-// ModelEvaluator model_evaluator;
 ChowLiuTreeBuilder<39> clt_builder;
 
 void Application::on_snapshot(dbtoaster::data_t& data) {
-    // on_end_processing(data, false);
-    std::cout << "snapshot" << std::endl;
+    on_end_processing(data, false);
 }
 
 void Application::on_begin_processing(dbtoaster::data_t& data) {
-    // double rmse = model_evaluator.compute_rmse(bgd_solver.params);
-    // std::cout << "RMSE: " << rmse << std::endl;
+
 }
 
 void Application::on_end_processing(dbtoaster::data_t& data, bool print_result) {
@@ -26,39 +22,21 @@ void Application::on_end_processing(dbtoaster::data_t& data, bool print_result) 
 
     const auto& cofactor_matrix = data.get_V_locn_IIWLC1();
     
-    data.serialize(std::cout, 0);
-
-    // bgd_solver.build_sigma_matrix(cofactor_matrix);
     clt_builder.build_mi_matrix(cofactor_matrix);
 
-    // print out the MI matrix
-    for (size_t i = 0; i < 39; i++) {
-        for (size_t j = 0; j < 39; j++) {
-            std::cout << clt_builder.mi_matrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // std::cout << "initial params: [ ";
-    // for (size_t i = 0; i < bgd_solver.NUM_PARAMS; i++) {
-    //     std::cout << std::setprecision(10) <<  bgd_solver.params[i] << " ";
+    // // print out the MI matrix
+    // for (size_t i = 0; i < 39; i++) {
+    //     for (size_t j = 0; j < 39; j++) {
+    //         std::cout << clt_builder.mi_matrix[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
     // }
-    // std::cout << " ]" << std::endl;
+    
+    // build the chow liu tree
+    clt_builder.build_chow_liu_tree();
+    if (print_result)
+        clt_builder.print_chow_liu_tree();
 
-    // size_t num_iterations = 0.0;
-    // double error = 0.0;
-    // bgd_solver.run(num_iterations, error);
-
-    // std::cout << "number of iterations: " << num_iterations ;
-
-    // std::cout << "params: [ ";
-    // for (size_t i = 0; i < bgd_solver.NUM_PARAMS; i++) {
-    //     std::cout << bgd_solver.params[i] << " ";
-    // }
-    // std::cout << " ]" << std::endl;
-
-    // double rmse = model_evaluator.compute_rmse(bgd_solver.params);
-    // std::cout << "RMSE: " << rmse << std::endl;
 }
 
-#endif /* APPLICATION_RETAILER_REGRESSION_HPP */
+#endif /* APPLICATION_RETAILER_CHOWLIUTREE_HPP */
--- examples/src/lib/clt.hpp
+++ examples/src/lib/clt.hpp
@@ -5,12 +5,110 @@ template <size_t SZ>
 struct ChowLiuTreeBuilder
 {
 
+    // MI matrix
     double mi_matrix[SZ][SZ]; // to store the matrix
+    
+
+    // for UnionFind
+    int parent[SZ];
+    int rank[SZ];
+    int num_sets;
+    
+    // chow liu tree
+    std::vector<std::pair<double, std::pair<int, int>>> tree_edges;
+
 
     ChowLiuTreeBuilder()
     {
         memset(mi_matrix, 0, sizeof(mi_matrix)); // initialize the matrix to 0
+        
+        // initialize the UnionFind
+        memset(parent, 0, sizeof(parent));
+        memset(rank, 0, sizeof(rank));
+
+        for (size_t i = 0; i < SZ; i++)
+        {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        num_sets = SZ;
     }
+    
+    // find the root of the set that x belongs to
+    int find(int x)
+    {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // path compression
+        return parent[x];
+    }
+    
+    // union the sets that x and y belong to
+    void union_sets(int x, int y)
+    {
+        int root_x = find(x);
+        int root_y = find(y);
+        
+        if (root_x == root_y) // already in the same set
+            return;
+        
+        if (rank[root_x] > rank[root_y]) {
+            parent[root_y] = root_x; // attach root_y to root_x
+            rank[root_x] += rank[root_y]; // update the rank of root_x
+        } else {
+            parent[root_x] = root_y; // attach root_x to root_y
+            rank[root_y] += rank[root_x]; // update the rank of root_y
+        }
+        num_sets--;
+    }
+    
+    // check if x and y are in the same set
+    bool is_same_set(int x, int y)
+    {
+        return find(x) == find(y);
+    }
+
+    // build the chow liu tree from the MI matrix
+    // using Kruskal's algorithm
+    void build_chow_liu_tree()
+    {
+        // sort the edges in the MI matrix in descending order
+        std::vector<std::pair<double, std::pair<int, int>>> edges;
+        for (size_t i = 0; i < SZ; i++)
+        {
+            for (size_t j = i+1; j < SZ; j++)
+            {
+                edges.push_back(std::make_pair(mi_matrix[i][j], std::make_pair(i, j)));
+            }
+        }
+        std::sort(edges.begin(), edges.end(), std::greater<std::pair<double, std::pair<int, int>>>());
+        
+
+        // build the chow liu tree
+        for (size_t i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i].second.first;
+            int v = edges[i].second.second;
+            if (!is_same_set(u, v))
+            {
+                // add the edge to the tree
+                tree_edges.push_back(edges[i]);
+                
+                // union the sets that u and v belong to
+                union_sets(u, v);
+            }
+        }
+    }
+    
+    // print the chow liu tree
+    void print_chow_liu_tree()
+    {
+        std::cout << std::endl << "Chow Liu Tree (<attr1> -- <attr2>: <weight>)" << std::endl;
+        for (size_t i = 0; i < tree_edges.size(); i++)
+        {
+            std::cout << tree_edges[i].second.first << " -- " << tree_edges[i].second.second << ": " << tree_edges[i].first << std::endl;
+        }
+    }
+    
 
     // build the MI matrix from the cofactor
     // the MI matrix is a SZ x SZ matrix
@@ -81,11 +179,6 @@ struct ChowLiuTreeBuilder
         }
     }
     
-    // TODO: build the chow liu tree from the MI matrix
-    void build_chow_liu_tree()
-    {
-        
-    }
 };
 
 #endif /* CHOWLIUTREEBUILDER_HPP */
