9f7ef5ee3e3ef9787f0f022dd544772ac64f42d0
Haozhe Zhang <imhaozhe@gmail.com>
2023-02-11 10:29:43 +0100

    static list

---
+++ examples/queries/favorita/favorita_static_list.sql
@@ -0,0 +1,35 @@
+IMPORT DTREE FROM FILE 'favorita.txt';
+
+CREATE TYPE RingRelation
+FROM FILE 'ring/ring_relational_opt.hpp'
+WITH PARAMETER SCHEMA (dynamic_concat);
+
+CREATE TABLE SALES(date int, store int, item int, unit_sales double, onpromotion int)
+FROM FILE './datasets/favorita/Sales.csv' LINE DELIMITED CSV(delimiter := ',');
+
+CREATE TABLE OIL(date int, oilprize double)
+FROM FILE './datasets/favorita/Oil.csv' LINE DELIMITED CSV(delimiter := ',');
+
+CREATE TABLE HOLIDAY(date int, holiday_type int, locale int, locale_id int, transferred int)
+FROM FILE './datasets/favorita/Holidays.csv' LINE DELIMITED CSV(delimiter := ',');
+
+CREATE TABLE TRANSACTIONS(date int, store int, transactions int) 
+FROM FILE './datasets/favorita/Transactions.csv' LINE DELIMITED CSV(delimiter := ',');
+
+CREATE TABLE STORES(store int, city int, state int, store_type int, cluster int) 
+FROM FILE './datasets/favorita/Stores.csv' LINE DELIMITED CSV(delimiter := ',');
+
+CREATE TABLE ITEMS(item int, family int, itemclass int, perishable int) 
+FROM FILE './datasets/favorita/Stores.csv' LINE DELIMITED CSV(delimiter := ',');
+
+SELECT SUM(
+    [lift<0>: RingRelation<[0,int]>](date) *
+    [lift<1>: RingRelation<[1,int]>](store) *
+    [lift<2>: RingRelation<[2,int,double,int]>](item, unit_sales, onpromotion) *
+    [lift<5>: RingRelation<[5,int,int,int]>](family, itemclass, perishable) *
+    [lift<8>: RingRelation<[8,int]>](transactions) *
+    [lift<9>: RingRelation<[9,int,int,int,int]>](city, state, store_type, cluster) *
+    [lift<13>: RingRelation<[13,double]>](oilprize) *
+    [lift<14>: RingRelation<[14,int,int,int,int]>](holiday_type, locale, locale_id, transferred)
+)
+FROM Sales NATURAL JOIN Oil NATURAL JOIN Holiday NATURAL JOIN Transactions NATURAL JOIN Stores NATURAL JOIN Items;
